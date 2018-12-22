bf68702b0cbf882df7832d8d1a6febe7119d28a4
dongxiaoping1015 <xiaoping_1015@163.com>
2018-12-22 22:56:58 +0800

    Spring security之后再看 权限搞得头痛

--- src/main/java/com/dong/readinglist/Reader.java
+++ src/main/java/com/dong/readinglist/Reader.java
@@ -1,8 +1,8 @@
 package com.dong.readinglist;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 @Entity
-public class Reader implements UserDetails {
+public class Reader {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -18,7 +18,6 @@ public class Reader implements UserDetails {
     private String fullname;
     private String password;
 
-    @Override
     public String getUsername() {
         return username;
     }
@@ -35,7 +34,6 @@ public class Reader implements UserDetails {
         this.fullname = fullname;
     }
 
-    @Override
     public String getPassword() {
         return password;
     }
@@ -47,28 +45,5 @@ public class Reader implements UserDetails {
     // UserDetails methods
 
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("READER"));
-    }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
