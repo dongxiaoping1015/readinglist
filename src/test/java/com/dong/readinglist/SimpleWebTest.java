0814ea1e772e31bfc059a171587d9942d00e3d90
dongxiaoping1015 <xiaoping_1015@163.com>
2018-12-23 16:14:08 +0800

    用随机端口启动服务器

--- src/test/java/com/dong/readinglist/SimpleWebTest.java
+++ src/test/java/com/dong/readinglist/SimpleWebTest.java
@@ -2,6 +2,7 @@ package com.dong.readinglist;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,14 +13,16 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ReadinglistApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = ReadinglistApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SimpleWebTest {
+    @Value("${local.server.port}")
+    private int port;
 
     @Test(expected = HttpClientErrorException.class)
     public void pageNotFound() {
        try {
            RestTemplate rest = new RestTemplate();
-           rest.getForObject("http://localhost:8081/bogusPage", String.class); // 发起GET请求
+           rest.getForObject("http://localhost:{port}/bogusPage", String.class, port); // 发起GET请求
            fail("Should result in HTTP 404");
        } catch (HttpClientErrorException e) {
            assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
