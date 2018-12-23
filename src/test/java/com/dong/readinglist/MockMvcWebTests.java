79354528ed59be3b8c602651805cd6765d7af137
dongxiaoping1015 <xiaoping_1015@163.com>
2018-12-23 15:17:04 +0800

    测试提交一本新书

--- src/test/java/com/dong/readinglist/MockMvcWebTests.java
+++ src/test/java/com/dong/readinglist/MockMvcWebTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -39,5 +40,32 @@ public class MockMvcWebTests {
                 .andExpect(view().name("readingList"))
                 .andExpect(model().attributeExists("books"))
                 .andExpect(model().attribute("books", is(empty())));
+    }
+
+    @Test
+    public void postBook() throws Exception {
+        mockMvc.perform(post("/readingList/dong")       // 执行POST请求
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED) // 将内容类型设置为运行应用程序时浏览器会发送的内容类型
+                .param("title", "BOOK TITLE")
+                .param("author", "BOOK AUTHOR")
+                .param("isbn", "1234567890")
+                .param("description", "DESCRIPTION"))
+                .andExpect(status().is3xxRedirection())             // 是否重定向
+                .andExpect(header().string("Location", "/readingList/dong"));
+
+        Book expectedBook = new Book();     // 配置期望的图书
+        expectedBook.setId(1L);
+        expectedBook.setReader("dong");
+        expectedBook.setTitle("BOOK TITLE");
+        expectedBook.setAuthor("BOOK AUTHOR");
+        expectedBook.setIsbn("1234567890");
+        expectedBook.setDescription("DESCRIPTION");
+
+        mockMvc.perform(get("/readingList/dong"))       // 执行GET请求
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", hasSize(1)))
+                .andExpect(model().attribute("books", contains(samePropertyValuesAs(expectedBook))));
     }
 }
